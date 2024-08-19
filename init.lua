dd123105292a52febe75501b98268bb908daccc6
Grady Wong <grady.w@xtaotech.com>
2024-08-19 14:14:09 +0800

    add inline git line history

--- init.lua
+++ init.lua
@@ -176,6 +176,39 @@ require('lazy').setup({
         changedelete = { text = '~' },
       },
     },
+    config = function()
+      require('gitsigns').setup {
+        current_line_blame = true,
+        on_attach = function(bufnr)
+          local function map(mode, lhs, rhs, opts)
+            opts = vim.tbl_extend('force', { noremap = true, silent = true }, opts or {})
+            vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+          end
+
+          -- Navigation
+          map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
+          map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
+
+          -- Actions
+          map('n', '<leader>hs', ':Gitsigns stage_hunk<CR>')
+          map('v', '<leader>hs', ':Gitsigns stage_hunk<CR>')
+          map('n', '<leader>hr', ':Gitsigns reset_hunk<CR>')
+          map('v', '<leader>hr', ':Gitsigns reset_hunk<CR>')
+          map('n', '<leader>hS', '<cmd>Gitsigns stage_buffer<CR>')
+          map('n', '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>')
+          map('n', '<leader>hR', '<cmd>Gitsigns reset_buffer<CR>')
+          map('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>')
+          map('n', '<leader>hb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
+          map('n', '<leader>hd', '<cmd>Gitsigns diffthis<CR>')
+          map('n', '<leader>hD', '<cmd>lua require"gitsigns".diffthis("~")<CR>')
+          map('n', '<leader>td', '<cmd>Gitsigns toggle_deleted<CR>')
+
+          -- Text object
+          map('o', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+          map('x', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+        end,
+      }
+    end,
   },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
@@ -361,22 +394,22 @@ require('lazy').setup({
       end, { desc = '[S]earch [N]eovim files' })
     end,
   },
-  {
-    'dhananjaylatkar/cscope_maps.nvim',
-    dependencies = {
-      'nvim-telescope/telescope.nvim',
-    },
-    config = function()
-      require('cscope_maps').setup {
-        disable_maps = false,
-        cscope = {
-          picker = 'telescope',
-          db_build_cmd_args = { '-Rbqkv' },
-          skip_input_prompt = true,
-        },
-      }
-    end,
-  },
+  -- {
+  --   'dhananjaylatkar/cscope_maps.nvim',
+  --   dependencies = {
+  --     'nvim-telescope/telescope.nvim',
+  --   },
+  --   config = function()
+  --     require('cscope_maps').setup {
+  --       disable_maps = false,
+  --       cscope = {
+  --         picker = 'telescope',
+  --         db_build_cmd_args = { '-Rbqkv' },
+  --         skip_input_prompt = true,
+  --       },
+  --     }
+  --   end,
+  -- },
 
   -- LSP Plugins
   {
@@ -565,6 +598,32 @@ require('lazy').setup({
             '8',
             '--malloc-trim',
             '--pch-storage=memory',
+            '--header-insertion=iwyu',
+            '--completion-style=detailed',
+            '--function-arg-placeholders',
+            '--fallback-style=llvm',
+          },
+          root_dir = function(fname)
+            return require('lspconfig.util').root_pattern(
+              'Makefile',
+              'configure.ac',
+              'configure.in',
+              'config.h.in',
+              'meson.build',
+              'meson_options.txt',
+              'build.ninja'
+            )(fname) or require('lspconfig.util').root_pattern('compile_commands.json', 'compile_flags.txt')(fname) or require('lspconfig.util').find_git_ancestor(
+              fname
+            )
+          end,
+
+          keys = {
+            { '<leader>ch', '<cmd>ClangdSwitchSourceHeader<cr>', desc = 'Switch Source/Header (C/C++)' },
+          },
+          init_options = {
+            usePlaceholders = true,
+            completeUnimported = true,
+            clangdFileStatus = true,
           },
         },
         gopls = {
