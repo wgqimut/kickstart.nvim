a0487844288e3f56a5c1ae7658360fa95377791e
Grady Wong <wgqimut@gmail.com>
2024-08-26 10:38:01 +0800

    add rust

--- init.lua
+++ init.lua
@@ -660,10 +660,13 @@ require('lazy').setup({
             },
             staticcheck = true,
             gofumpt = true,
+            navigation = {
+              symbolScope = 'workspace',
+            },
           },
         },
-        --pyright = {},
-        -- rust_analyzer = {},
+        pyright = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
