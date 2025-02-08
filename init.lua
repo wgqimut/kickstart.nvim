88d9447762ba4da380847f83c1b81a0ae1c789c7
Grady Wong <grady.w@xtaotech.com>
2025-02-08 17:20:28 +0800

    from clangd to ccls for C code

--- init.lua
+++ init.lua
@@ -654,41 +654,6 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        clangd = {
-          cmd = {
-            'clangd',
-            '--background-index',
-            '--clang-tidy',
-            '-j',
-            '8',
-            '--malloc-trim',
-            '--pch-storage=memory',
-            '--header-insertion=iwyu',
-            '--completion-style=detailed',
-            '--function-arg-placeholders',
-            '--fallback-style=llvm',
-            '--offset-encoding=utf-16',
-          },
-          root_dir = function(fname)
-            return require('lspconfig.util').root_pattern(
-              'Makefile',
-              'configure.ac',
-              'configure.in',
-              'config.h.in',
-              'meson.build',
-              'meson_options.txt',
-              'build.ninja'
-            )(fname) or require('lspconfig.util').root_pattern('compile_commands.json', 'compile_flags.txt')(fname) or require('lspconfig.util').find_git_ancestor(
-              fname
-            )
-          end,
-
-          init_options = {
-            usePlaceholders = true,
-            completeUnimported = true,
-            clangdFileStatus = true,
-          },
-        },
         gopls = {
           settings = {
             analyses = {
@@ -760,6 +725,14 @@ require('lazy').setup({
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
             require('lspconfig')[server_name].setup(server)
           end,
+        },
+      }
+
+      require('lspconfig').ccls.setup {
+        init_options = {
+          cache = {
+            directory = '.ccls-cache',
+          },
         },
       }
     end,
